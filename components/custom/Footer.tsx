4b57700b2fdd4f5e1b6652002182c0d6d14af735
fine-pine <lsm3645@g.skku.edu>
2024-11-30 19:31:38 +0900

    refactor: move components

---
+++ components/custom/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Component() {
+  return (
+    <footer className="mt-16 text-xs text-neutral-500">
+      Kevin Film ⓒ All Rights Reserved
+    </footer>
+  );
+}
---
+++ components/custom/Header.tsx
@@ -0,0 +1,23 @@
+import { ROUTER } from "../../app/constants";
+import Link from "next/link";
+
+export default function Component() {
+  return (
+    <header className="mt-16 flex justify-between">
+      <Link className="text-2xl font-medium" href="/">
+        Kevin Film
+      </Link>
+      <nav>
+        <ul className="flex gap-8 text-base">
+          {ROUTER.map((route) => (
+            <li>
+              <Link key={route.title} href={route.path}>
+                {route.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
