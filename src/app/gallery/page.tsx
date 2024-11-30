6d6a47a0c5d65cfd29ff672ea812c6af229205ca
fine-pine <lsm3645@g.skku.edu>
2024-12-01 03:14:29 +0900

    feat: add image tag filtering

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,22 @@
 import MasonryGridWrapper from "@/src/components/custom/MasonryGridWithDialog";
 import { createClient } from "@/src/utils/supabase/server";
+import { TAGS } from "@/src/constants";
 
-export default async function Component() {
+interface Props {
+  searchParams: Promise<{
+    tag: (typeof TAGS)[];
+  }>;
+}
+
+export default async function Component({ searchParams }: Props) {
+  const { tag } = await searchParams;
   const supabase = await createClient();
-  const { data: images } = await supabase.from("images").select();
+
+  let query = supabase.from("images").select();
+  if (tag) {
+    query = query.contains("tags", [tag]);
+  }
+  const { data: images } = await query;
 
   return <>{images && <MasonryGridWrapper images={images} />}</>;
 }
