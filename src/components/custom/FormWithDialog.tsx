bc7e4d932bc1555f44d74200855720deb5774d14
fine-pine <lsm3645@g.skku.edu>
2024-12-01 16:52:42 +0900

    feat: 갤러리 사진 작성 기능 구현

--- src/components/custom/FormWithDialog.tsx
+++ src/components/custom/FormWithDialog.tsx
@@ -4,50 +4,106 @@ import { Button } from "@/src/components/ui/button";
 import { Input } from "@/src/components/ui/input";
 import { Label } from "@/src/components/ui/label";
 import { Textarea } from "@/src/components/ui/textarea";
-import { FormEventHandler, MouseEventHandler, useState } from "react";
-import { useForm } from "react-hook-form";
+import {
+  ChangeEvent,
+  FormEventHandler,
+  MouseEventHandler,
+  useState,
+} from "react";
 import { SubmitButton } from "../submit-button";
+import UppyApp from "../uppy/UppyApp";
+import { Badge } from "../ui/badge";
+import { createClient } from "@/src/utils/supabase/client";
+import dayjs from "dayjs";
+
+export interface ImageForm {
+  image_url: string;
+  title: string;
+  description: string;
+  tags: string[];
+  filmed_at: Date | null;
+}
 
 export default function Component() {
+  const defaultValues: ImageForm = {
+    image_url: "",
+    title: "",
+    description: "",
+    tags: [],
+    filmed_at: null,
+  };
+
   const [showDialog, setShowDialog] = useState(false);
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      tags: [],
-      title: "",
-      description: "",
-      image: null,
-      filmed_at: new Date(),
-    },
-  });
+  const [formData, setFormData] = useState<ImageForm>(defaultValues);
+  const [tag, setTag] = useState("");
+  const [supabase] = useState(createClient());
+  const [error, setError] = useState<string | null>(null);
 
-  const handleFormSubmit: FormEventHandler<HTMLFormElement> = handleSubmit(
-    () => {
-      setShowDialog(true);
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleAddTag: MouseEventHandler = (e) => {
+    e.preventDefault();
+    const value = tag.trim();
+    setTag("");
+    if (!value) return;
+    if (value === "") return;
+    if (formData.tags.includes(value)) return;
+    setFormData((prev) => ({
+      ...prev,
+      tags: [...prev.tags, value],
+    }));
+  };
+
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    if (formData.image_url === "") {
+      setError("Image is required");
+      return;
     }
-  );
+    if (formData.title === "") {
+      setError("Title is required");
+      return;
+    }
+    if (formData.description === "") {
+      setError("Description is required");
+      return;
+    }
+    if (formData.tags.length === 0) {
+      setError("Tags are required");
+      return;
+    }
+    if (formData.filmed_at === null) {
+      setError("Filmed at is required");
+      return;
+    }
 
-  const handleDialogConfirm = handleSubmit((formData) => {
-    const data = new FormData();
+    setShowDialog(true);
+  };
 
-    // TODO: add attributes
-    data.append("image", formData.image!);
-    data.append("description", formData.description);
-
-    // TODO: supabase image upload
-    // TODO: supabase row insert
-
+  const handleDialogConfirm = () => {
+    supabase.from("images").insert(formData);
+    setTag("");
+    setFormData(defaultValues);
     setShowDialog(false);
-    reset();
-  });
+  };
 
   const handleDialogCancel: MouseEventHandler = (e) => {
-    e.preventDefault();
     setShowDialog(false);
+  };
+
+  const setImageUrl = (url: string) => {
+    setFormData((prev) => ({
+      ...prev,
+      image_url: url,
+    }));
   };
 
   return (
@@ -56,50 +112,92 @@ export default function Component() {
         className="flex-1 flex flex-col min-w-64"
         onSubmit={handleFormSubmit}
       >
-        {
-          // TODO: add attributes
-        }
-        <div className="flex flex-col gap-2 [&>p]:mb-3 mt-8">
+        <div className="flex flex-col gap-2 mt-8">
           <Label>Image</Label>
-          <Input
-            type="file"
-            accept="image/*"
-            {...register("image", {
-              required: "Image is required.",
-            })}
+          <UppyApp
+            className="mb-3"
+            bucketName="finepine"
+            folderPath="kevin_flim_images"
+            setImageUrl={setImageUrl}
           />
-          {errors.image && (
-            <p className="text-xs text-destructive">{errors.image.message}</p>
-          )}
-          <Label>Description</Label>
+
+          <Label htmlFor="title">Title</Label>
+          <Input
+            className="mb-3"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+          />
+
+          <Label className="mb-3" htmlFor="description">
+            Description
+          </Label>
           <Textarea
-            {...register("description", {
-              required: "Description is required.",
-            })}
-          ></Textarea>
-          {errors.description && (
-            <p className="text-xs text-destructive">
-              {errors.description.message}
-            </p>
-          )}
+            className="mb-3"
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            rows={4}
+          />
+
+          <Label htmlFor="tags">Tags</Label>
+          <div className="flex gap-2">
+            <Input
+              placeholder="input tags"
+              value={tag}
+              onChange={(e) => setTag(e.currentTarget.value)}
+            />
+            <Button onClick={handleAddTag}>Add</Button>
+          </div>
+          <div className="flex flex-wrap gap-2 mb-3">
+            {formData.tags.map((tag, idx) => (
+              <Badge
+                key={idx}
+                className="cursor-pointer"
+                variant="outline"
+                onClick={() =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    tags: prev.tags.filter((_, i) => i !== idx),
+                  }))
+                }
+              >
+                {tag}
+              </Badge>
+            ))}
+          </div>
+
+          <Label htmlFor="filmed_at">Filemed At</Label>
+          <Input
+            className="mb-3"
+            id="filmed_at"
+            name="filmed_at"
+            type="datetime-local"
+            value={dayjs(formData.filmed_at).format("YYYY-MM-DDTHH:mm")}
+            onChange={handleChange}
+          />
+
           <SubmitButton pendingText="Uploading...">Upload</SubmitButton>
+          {error && <p className="text-xm text-destructive">{error}</p>}
         </div>
       </form>
 
       {showDialog && (
         <>
           <div className="fixed flex flex-col gap-8 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-background text-sm px-3 py-2 rounded-md z-10">
-            <p>정말로 제출하시겠습니까?</p>
+            <p></p>
             <div className="flex gap-4">
               <Button
                 className="flex-1"
                 variant="outline"
                 onClick={handleDialogCancel}
               >
-                취소
+                Cancel
               </Button>
               <Button className="flex-1" onClick={handleDialogConfirm}>
-                확인
+                Confirm
               </Button>
             </div>
           </div>
